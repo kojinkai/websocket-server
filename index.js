aaa0bf04c2f8348955fcd0a64b79eecd3f47857b
Lewis Nixon <lewcfn@gmail.com>
2020-06-30 17:41:09 +0200

    setup websocket server

---
+++ index.js
@@ -0,0 +1,15 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 3001 });
+
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    console.log(`Received message => ${message}`);
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+  ws.send("socket setup");
+});
